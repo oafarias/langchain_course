ff0f2f8b0b09c64d4dfcaf9bedf6a37f718f86a4
Alef Farias <alef.alves1800@gmail.com>
2026-05-28 15:35:06 -0300

    Aula 02: Conectando com API da OpenAI via LangChain

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
+from langchain_openai import ChatOpenAI
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 api_key=os.getenv("OPENAI_API_KEY")
 
 numero_dias = 7
 numero_criancas = 2
-atividade = "música"
+atividade = "praia"
 
-prompt = f"Crie um roteiro de viajem de {numero_dias} dias, para uma família com {numero_criancas} criancas, que gostaria de {atividade}"
+prompt = f"Crie um roteiro de viagens, para um periodo de {numero_dias} dias, para uma familia com {numero_criancas} crianças, que buscam atividades relacionados a {atividade}."
 
-cliente = OpenAI(api_key=api_key)
-
-resposta = cliente.chat.completions.create(
+modelo = ChatOpenAI(
     model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": "Você é um assistente de roteiro de viagens."
-        },
-        {
-            "role": "user",
-            "content": prompt
-        }
-    ]
+    temperature=0.5,
+    api_key=api_key
 )
-resposta_texto = resposta.choices[0].message.content
-print(resposta_texto)
+
+resposta = modelo.invoke(prompt)
+print(resposta.content)

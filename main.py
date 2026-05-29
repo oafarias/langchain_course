79d88b4d449012ee9da6cf589bb3892475bb73f9
Alef Farias <alef.alves1800@gmail.com>
2026-05-28 21:42:54 -0300

    Aula 5: Definindo a resposta do modelo como um objeto JSON

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser #, StrOutputParser
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
+from langchain.globals import set_debug
 import os
+
+set_debug(True)
 
 load_dotenv()
 api_key=os.getenv("OPENAI_API_KEY")
 
+class Destino(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    motivo:str = Field("motivo pelo qual é interessante visitar essa cidade")
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
 prompt_cidade = PromptTemplate(
     template = """
     Sugira uma cidade dao o meu interesse por {interesse}.
+
+    {formato_de_saida}
     """,
-    input_variables=["interesse"]
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
 )
 
 modelo = ChatOpenAI(
@@ -20,7 +33,7 @@ modelo = ChatOpenAI(
     api_key=api_key
 )
 
-cadeia = prompt_cidade | modelo | StrOutputParser()
+cadeia = prompt_cidade | modelo | parseador
 
 resposta = cadeia.invoke(
     {

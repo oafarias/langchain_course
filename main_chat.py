e1fcdcf46ecc361aaefde89214ef0432a495cd58
Alef Farias <alef.alves1800@gmail.com>
2026-05-30 23:04:16 -0300

    Aula 09: Adicionando memoria ao chat com LangChain

--- main_chat.py
+++ main_chat.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
@@ -23,12 +25,32 @@ prompt_sugestao = ChatPromptTemplate.from_messages(
 
 cadeia = prompt_sugestao | modelo | StrOutputParser()
 
+memoria = {}
+sessao = "aula_langchain_alura"
+
+def historico_por_sessao(sessao : str):
+    if sessao not in memoria:
+        memoria[sessao] = InMemoryChatMessageHistory()
+    return memoria[sessao]
+
 lista_perguntas = [
     "Quero visitar um lugar no Brasil, famoso por praias e cultura. Pode sugerir?",
     "Qual a melhor época do ano para ir?"
 ]
 
+cadeia_com_memoria = RunnableWithMessageHistory(
+    runnable=cadeia,
+    get_session_history=historico_por_sessao,
+    input_messages_key="query",
+    history_messages_key="historico"
+)
+
 for uma_pergunta in lista_perguntas:
-    resposta = modelo.invoke(uma_pergunta)
+    resposta = cadeia_com_memoria.invoke(
+        {
+            "query" : uma_pergunta
+        },
+        config={"session_id":sessao}
+    )
     print("Usuário: ", uma_pergunta)
-    print("IA: ", resposta.content, "\n")
+    print("IA: ", resposta, "\n")

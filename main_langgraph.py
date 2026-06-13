7811ef312045ccb866cf88059937aeb2897ed58a
Alef Farias <alef.alves1800@gmail.com>
2026-06-13 15:42:26 -0300

    Aula 12: Orquestrando assistentes comLangGraph

--- main_langgraph.py
+++ main_langgraph.py
@@ -3,6 +3,9 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from typing import Literal, TypedDict
+from langgraph.graph import StateGraph, START, END
+from langchain_core.runnables import RunnableConfig
+import asyncio
 import os
 
 load_dotenv()
@@ -43,10 +46,39 @@ prompt_roteador = ChatPromptTemplate.from_messages(
 
 roteador = prompt_roteador | modelo.with_structured_output(Rota)
 
-def responda(pergunta :str):
-    rota = roteador.invoke({"query":pergunta})["destino"]
-    if rota == "praia":
-        return cadeia_praia.invoke({"query": pergunta})
-    return cadeia_montanha.invoke({"query": pergunta})
+class Estado(TypedDict):
+    query: str
+    destino: Rota
+    resposta: str
 
-print(responda("Quero surfar em um local quente"))
+async def no_roteador(estado: Estado, config=RunnableConfig):
+    return {"destino": await roteador.ainvoke({"query":estado["query"]}, config)}
+
+async def no_praia(estado: Estado, config=RunnableConfig):
+    return {"resposta": await cadeia_praia.ainvoke({"query":estado["query"]}, config)}
+
+async def no_montanha(estado: Estado, config=RunnableConfig):
+    return {"resposta": await cadeia_montanha.ainvoke({"query":estado["query"]}, config)}
+
+def escolher_no(estado:Estado) -> Literal["praia", "montanha"]:
+    return "praia" if estado["destino"]["destino"] == "praia" else "montanha"
+
+grafo = StateGraph(Estado)
+grafo.add_node("rotear", no_roteador)
+grafo.add_node("praia", no_praia)
+grafo.add_node("montanha", no_montanha)
+
+grafo.add_edge(START, "rotear")
+grafo.add_conditional_edges("rotear", escolher_no)
+grafo.add_edge("praia", END)
+grafo.add_edge("montanha", END)
+
+app = grafo.compile()
+
+async def main():
+    resposta = await app.ainvoke(
+        {"query":"Quero visitar um lugar no Brasil famoso por praias e cultura"}
+    )
+    print(resposta["resposta"])
+
+asyncio.run(main())
